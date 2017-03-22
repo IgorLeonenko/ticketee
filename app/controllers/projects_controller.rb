1c252f6c6b75d13f69f20f2bbf6364596ca4697a
Igor Leonenko <igrleon@gmail.com>
2017-03-23 00:06:19 +0100

    implement delete project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_action :find_project, only: [:show, :edit, :update]
+  before_action :find_project, only: [:show, :edit, :update, :destroy]
 
   def index
     @projects = Project.all
@@ -7,6 +7,17 @@ class ProjectsController < ApplicationController
 
   def new
     @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:notice] = "Project has been created."
+      redirect_to @project
+    else
+      flash.now[:alert] = "Project has not been created."
+      render :new
+    end
   end
 
   def show; end
@@ -23,15 +34,11 @@ class ProjectsController < ApplicationController
     end
   end
 
-  def create
-    @project = Project.new(project_params)
-    if @project.save
-      flash[:notice] = "Project has been created."
-      redirect_to @project
-    else
-      flash.now[:alert] = "Project has not been created."
-      render :new
-    end
+  def destroy
+    @project.destroy
+
+    flash[:notice] = "Project has been deleted."
+    redirect_to projects_path
   end
 
 
