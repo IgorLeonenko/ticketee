949d507f61d309f6441393fefbcf8c480b882d6c
Igor Leonenko <igrleon@gmail.com>
2017-05-05 11:14:53 +0200

    Link tickets and users upon ticket creation

--- spec/features/creating_ticket_spec.rb
+++ spec/features/creating_ticket_spec.rb
@@ -1,10 +1,13 @@
 require "rails_helper"
 
 RSpec.feature "Users can create new tickets" do
+  let(:user) { FactoryGirl.create(:user) }
+
   before do
     project = FactoryGirl.create(:project, name: "Internet Explorer")
     visit project_path(project)
     click_link "New Ticket"
+    login_as(user)
   end
 
   scenario "with valid attributes" do
@@ -28,4 +31,15 @@ RSpec.feature "Users can create new tickets" do
     expect(page).to have_content "Ticket has not been created."
     expect(page).to have_content "Description is too short"
   end
+
+  scenario "with valid attributes" do
+    fill_in "Name", with: "Non-standards compliance"
+    fill_in "Description", with: "My pages are ugly!"
+    click_button "Create Ticket"
+    expect(page).to have_content "Ticket has been created."
+    within("#ticket") do
+      expect(page).to have_content "Author: #{user.email}"
+    end
+  end
+
 end
